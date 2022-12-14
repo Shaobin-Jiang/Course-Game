ff7a067028b80f74800aaaa6ad7ce1e26e453132
Shaobin-Jiang <shaobin-jiang@outlook.com>
2022-12-14 21:00:27 +0800

    create: webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './main.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
