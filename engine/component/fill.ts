49ca5741c2e0751c8095c1b802f04090fb2a731c
Shaobin-Jiang <shaobin-jiang@outlook.com>
2023-01-01 15:27:14 +0800

    create: component for filling a rectangular area

---
+++ engine/component/fill.ts
@@ -0,0 +1,26 @@
+import {Rect} from '../geometry';
+import {GameEvent} from '../util';
+import {Component} from './component';
+
+export class Fill extends Component {
+    /**
+     * Fill a rectangular area
+     *
+     * @param rect The target area
+     * @param color The color with which to fill the area
+     */
+    constructor(private rect: Rect, private color: string | CanvasGradient | CanvasPattern = '#ffffff') {
+        super();
+    }
+
+    public draw(canvas: HTMLCanvasElement, canvas_rect: Rect, event: GameEvent): void {
+        let context: CanvasRenderingContext2D = canvas.getContext('2d');
+
+        context.save();
+
+        context.fillStyle = this.color;
+        context.fillRect(...this.rect.spread());
+
+        context.restore();
+    }
+}
