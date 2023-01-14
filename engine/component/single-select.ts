3ee87a315ccfe5e0e4f58f71a14c3a7cb8bf621b
Shaobin-Jiang <shaobin-jiang@outlook.com>
2023-01-14 20:31:17 +0800

    create: component for multiple choice

---
+++ engine/component/single-select.ts
@@ -0,0 +1,56 @@
+import {GameEvent} from '../util';
+import {Rect} from '../geometry';
+import {Component} from './component';
+import {Select} from './select';
+
+export class SingleSelect extends Component {
+    constructor(private children: Array<Select>) {
+        super();
+
+        let index: number = 0;
+        for (let select of this.children) {
+            let id: number = index;
+            select.onclick = () => {
+                if (select.selected) {
+                    select.selected = false;
+                    this.selected = -1;
+                } else {
+                    select.selected = true;
+                    this.selected = id;
+                    for (let i = 0; i < this.children.length; i++) {
+                        if (i != id) {
+                            this.children[i].selected = false;
+                        }
+                    }
+                }
+            };
+            index++;
+        }
+    }
+
+    static async from(params: Array<any>): Promise<() => SingleSelect> {
+        let func_list: Array<() => Select> = [];
+
+        for (let param of params) {
+            func_list.push(await Select.from(param));
+        }
+
+        return () =>
+            new SingleSelect(
+                func_list.map(function (value: () => Select): Select {
+                    return value();
+                })
+            );
+    }
+
+    // Unfreeeze the component
+    public prevent_freeze: boolean = true;
+
+    public selected: number = -1;
+
+    public draw(canvas: HTMLCanvasElement, rect: Rect, event: GameEvent): void {
+        for (let select of this.children) {
+            select.draw(canvas, rect, event);
+        }
+    }
+}
